9a22a80f93bdca0266a9a166f4a2c17923d7c10d
Vadim-V-S <vaddimsav@gmail.com>
2021-04-29 23:27:15 +0500

    Добавьте файлы проекта.

---
+++ SF_App_5.6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SF_App_5._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
